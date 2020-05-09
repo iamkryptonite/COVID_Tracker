1c2c8a05f206f2d9bd61636d0082738718b67e76
iamkryptonite <iamkryptonite@users.noreply.github.com>
2020-05-10 00:54:31 +0530

    test deploy 3

--- index.js
+++ index.js
@@ -25,43 +25,43 @@ app.use(cors());
 // 		recovered.push(element["recovered"]);		
 // 	});
 // }
-// app.get("/",function(req,res){
-//     // getData();
-//     var url="https://api.covid19india.org/data.json";
-//     url = url.replace(" ", "%20");
-// 	request({
-// 		uri:url,
-// 		qs:{
-// 			api_key: '20200509194709'
-// 		}
-// 	},function(err,response,body){
-// 		if(!err && response.statusCode===200){
-// 			var data=JSON.parse(body);
-// 			var statewise=data.statewise;
-// 			// console.log(statewise);
-// 			// res.send("working")
-// 			res.render("homepage",{data:data});
-// 		}
-// 		else{
-// 			console.log(err)
-// 			console.log("Problem")
-// 			res.send("not working")
-
-// 		}
-		
-// 	});
-// 	// res.send("not working")	
-// })
-// app.get("/graph",function(req,res){
-// 	res.render("graph");
-// });
-// app.get("/:id",function(req,res){
-// 	res.render("show",{code:req.params.id});
-// });
-
 app.get("/",function(req,res){
-	res.send("working")
+    // getData();
+    var url="https://api.covid19india.org/data.json";
+    url = url.replace(" ", "%20");
+	request({
+		uri:url,
+		qs:{
+			api_key: '20200509194709'
+		}
+	},function(err,response,body){
+		if(!err && response.statusCode===200){
+			var data=JSON.parse(body);
+			var statewise=data.statewise;
+			// console.log(statewise);
+			// res.send("working")
+			res.render("homepage",{data:data});
+		}
+		else{
+			console.log(err)
+			console.log("Problem")
+			res.send("not working")
+
+		}
+		
+	});
+	// res.send("not working")	
 })
+app.get("/graph",function(req,res){
+	res.render("graph");
+});
+app.get("/:id",function(req,res){
+	res.render("show",{code:req.params.id});
+});
+
+// app.get("/",function(req,res){
+// 	res.send("working")
+// })
 //===============================================================================================================
 app.listen(3000,function(){
 	console.log("server is live")
